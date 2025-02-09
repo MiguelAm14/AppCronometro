bbbd13cbec9f4f2a17f2672c9b48e838010afe19
MiguelAm14 <alu.23130550@correo.itlalaguna.edu.mx>
2025-02-08 21:36:20 -0600

    Add project files.

---
+++ AppCronometro/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace AppCronometro
+{
+    partial class FrmCronometro
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblContador = new Label();
+            btnAccionar = new Button();
+            btnReiniciar = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // lblContador
+            // 
+            lblContador.AutoSize = true;
+            lblContador.Font = new Font("7 SEGMENTAL DIGITAL DISPLAY", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblContador.ForeColor = SystemColors.ControlDarkDark;
+            lblContador.Location = new Point(50, 33);
+            lblContador.Name = "lblContador";
+            lblContador.Size = new Size(438, 94);
+            lblContador.TabIndex = 0;
+            lblContador.Text = "00:00:00";
+            lblContador.TextAlign = ContentAlignment.MiddleCenter;
+            lblContador.Click += lblContador_Click;
+            // 
+            // btnAccionar
+            // 
+            btnAccionar.BackColor = Color.Green;
+            btnAccionar.Font = new Font("Roboto", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAccionar.ForeColor = SystemColors.ButtonHighlight;
+            btnAccionar.Location = new Point(134, 156);
+            btnAccionar.Name = "btnAccionar";
+            btnAccionar.Size = new Size(113, 47);
+            btnAccionar.TabIndex = 1;
+            btnAccionar.Text = "Iniciar";
+            btnAccionar.UseVisualStyleBackColor = false;
+            btnAccionar.Click += btnAccionar_Click;
+            // 
+            // btnReiniciar
+            // 
+            btnReiniciar.Font = new Font("Roboto", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnReiniciar.ForeColor = SystemColors.ControlDarkDark;
+            btnReiniciar.Location = new Point(276, 156);
+            btnReiniciar.Name = "btnReiniciar";
+            btnReiniciar.Size = new Size(113, 47);
+            btnReiniciar.TabIndex = 3;
+            btnReiniciar.Text = "Reiniciar";
+            btnReiniciar.UseVisualStyleBackColor = true;
+            btnReiniciar.Click += btnReiniciar_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // FrmCronometro
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(541, 231);
+            Controls.Add(btnReiniciar);
+            Controls.Add(btnAccionar);
+            Controls.Add(lblContador);
+            Name = "FrmCronometro";
+            Text = "Cronómetro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void lblContador_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private Label lblContador;
+        private Button btnAccionar;
+        private Button btnReiniciar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ AppCronometro/Form1.cs
@@ -0,0 +1,67 @@
+namespace AppCronometro
+{
+    public partial class FrmCronometro : Form
+    {
+        // Declaración de variables de tiempo, y control del cronometro
+        int horas, minutos, segundos;
+        Boolean play;
+
+        public FrmCronometro()
+        {
+            InitializeComponent();
+            horas = minutos = segundos = 0;
+            play = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string tiempo;
+            // Aumentar los segundos, minutos y horas de acuerdo con el timer.
+            segundos++;
+            if (segundos == 60)
+            {
+                segundos = 0;
+                minutos++;
+            }
+            if (minutos == 60)
+            {
+                minutos = 0;
+                horas++;
+            }
+
+            tiempo = horas.ToString("00") + ":" + minutos.ToString("00") + ":" + segundos.ToString("00");
+            lblContador.Text = tiempo;
+
+        }
+
+        private void btnAccionar_Click(object sender, EventArgs e)
+        {
+            // Si el cronometro esta en marcha, el botón cambia a parar.
+            if (play == true)
+            {
+                timer1.Stop();
+                play = false;
+                btnAccionar.BackColor = Color.Green;
+                btnAccionar.Text = "Iniciar";
+                return;
+            }
+
+            // Si el cronometro esta parado, cambia a iniciar.
+            timer1.Enabled = true;
+            timer1.Start();
+            play = true;
+            btnAccionar.BackColor = Color.Red;
+            btnAccionar.Text = "Parar";
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            // Se detiene el timer y se reinician las horas, minutos y segundos.
+            timer1.Stop();
+            horas = minutos = segundos = 0;
+            lblContador.Text = "00:00:00";
+        }
+
+
+    }
+}
